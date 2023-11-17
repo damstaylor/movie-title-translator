d8c629af41112aae66d401312ffe3006ef0f4481
damstaylor <damiensabo@live.fr>
2023-11-17 10:53:48 +0100

    Add basic main feature with API consuming

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
+import {useCallback, useEffect, useState} from 'react';
+
+const API_KEY = 'c3ee2bb51e36fe8ee23b3c051a560200';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const BASE_URL_IMG = 'https://image.tmdb.org/t/p/';
+const NB_RESULTS = 5;
 
 function App() {
+  const [searchTitle, setSearchTitle] = useState('');
+  const [sourceLang, setSourceLang] = useState('en-US');
+  const [destinationLang, setDestinationLang] = useState('fr-CA');
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const fetchMovies = useCallback(async () => {
+    const url = `${BASE_URL}search/movie?api_key=${API_KEY}&query=${searchTitle}&language=${sourceLang}&page=1&include_adult=false`;
+    const req = new Request(url, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    });
+    const response = await fetch(req);
+    return response.json().then(res => {
+      setMovies(res.results.slice(0, NB_RESULTS));
+    }).catch(err => {
+      console.error(err);
+    });
+  }, [searchTitle, sourceLang]);
+  const fetchMovieById = useCallback(async (id) => {
+    const url = `${BASE_URL}movie/${id}?api_key=${API_KEY}&language=${destinationLang}`;
+    const req = new Request(url, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    });
+    const response = await fetch(req);
+    return response.json().then(res => {
+      console.log(res);
+      setSelectedMovie(res);
+    }).catch(err => {
+      console.error(err);
+    });
+  }, [destinationLang]);
+  const getMoviesUl = () => {
+    return (<ul>
+      {movies.map((x, i) => (
+        <li key={i} onClick={() => fetchMovieById(x.id)}>
+          <img src={getMoviePosterURL(x)} alt={x.title}/>
+          <span>{`${x.title} (${x.release_date.slice(0, 4)})`}</span>
+        </li>
+      ))}
+    </ul>);
+  };
+  const getSelectedMovieDetails = () => {
+    return selectedMovie ? (
+      <>
+        <img src={getMoviePosterURL(selectedMovie)} alt={selectedMovie.title}/>
+        <span>{selectedMovie.title}</span>
+      </>
+    ) : null
+  };
+  const getMoviePosterURL = (movie, width = 200) => {
+    return `${BASE_URL_IMG}w${width}${movie.poster_path}`;
+  };
+  const onChangeSourceLang = (event) => {
+    setSourceLang(event.target.value);
+  };
+  const onChangeDestinationLang = (event) => {
+    setDestinationLang(event.target.value);
+  };
+  const onTitleInput = (event) => {
+    const val = event.target.value;
+    if (!val) {
+      setMovies([]);
+      return;
+    }
+    setSearchTitle(event.target.value);
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, [sourceLang, fetchMovies]);
+  useEffect(() => {
+    if (selectedMovie) {
+      fetchMovieById(selectedMovie.id);
+    }
+  }, [destinationLang, fetchMovieById]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div>Source lang: {sourceLang}</div>
+        <div>
+          <span>Search movie or TV show title in</span>
+          <select defaultValue="en-US" onChange={(event) => onChangeSourceLang(event)}>
+            <option value="en-US">English (USA)</option>
+            <option value="fr-CA">Français (Canada)</option>
+            <option value="fr-FR">Français (France)</option>
+          </select>
+          <input placeholder="Type here" value={searchTitle} onInput={onTitleInput}/>
+        </div>
+        <div>
+          <span>Translate to:</span>
+          <select defaultValue="fr-CA" onChange={(event) => onChangeDestinationLang(event)}>
+            <option value="en-US">English (USA)</option>
+            <option value="fr-CA">Français (Canada)</option>
+            <option value="fr-FR">Français (France)</option>
+          </select>
+        </div>
+      </div>
+      {movies ? getMoviesUl() : null}
+      {getSelectedMovieDetails()}
+      <div>Destination lang: {destinationLang}</div>
     </div>
   );
 }
